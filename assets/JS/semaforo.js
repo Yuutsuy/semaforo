ff0a39b07ebb5c8186be8073cc9aefa286b2b745
Yuutsuy <kiraidase@gmail.com>
2022-04-19 12:18:13 -0300

    Adicionado h1 na estrutura h1, feito javascript para exibir contador dentro das cores do semaforo quando acesas

--- assets/JS/semaforo.js
+++ assets/JS/semaforo.js
@@ -13,14 +13,41 @@ var mudar = function() {
        cont++;
        console.log(cont);
        if(cont <= 15) {
-           
+           redCircle.classList.remove('grey');
+           redCircle.classList.add('divRed');
+           redCircle.innerText = (cont);
+           if(cont <= 9) {
+               redCircle.innerText = (`0${cont}`);
+           }else{
+            redCircle.innerText = (cont);  
+           };
        }else if(cont <= 19) {
-           console.log('yellowDentro');
+           redCircle.innerText = "";
+           redCircle.classList.remove('divRed');
+           redCircle.classList.add('grey');
+           yellowCircle.classList.remove('grey');
+           yellowCircle.classList.add('divYellow');
+           if(cont - 15 <= 9) {
+               yellowCircle.innerText = (`0${cont - 15}`);
+            }else{
+                yellowCircle.innerText = (cont - 15);  
+            };
        }else if(cont <= 39) {
-           console.log('greenDentro');
+           yellowCircle.innerText = "";
+           yellowCircle.classList.remove('divYellow');
+           yellowCircle.classList.add('grey');
+           greenCircle.classList.remove('grey');
+           greenCircle.classList.add('divGreen');
+           if(cont - 19 <= 9) {
+               greenCircle.innerText = (`0${cont - 19}`);
+            }else{
+                greenCircle.innerText = (cont - 19);  
+            };
        }else{
            cont = 0;
-           console.log('Resete');
+           greenCircle.classList.remove('divGreen');
+           greenCircle.classList.add('grey');
+           greenCircle.innerText = "";
        }
    }, 500);
 };
